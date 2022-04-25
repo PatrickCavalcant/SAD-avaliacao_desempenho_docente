c36e3462c6aa1a67462ae936dfd834a2ebcab3f3
Patrick Cavalcante <54637677+PatrickCavalcant@users.noreply.github.com>
2022-04-25 20:13:00 -0300

    SAD

---
+++ sad-ifg-client/src/app/autenticacao/login/components/login/login.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { Login } from '../../models';
+import { LoginService } from '../../services';
+
+@Component({
+  selector: 'app-login-pf',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private loginService: LoginService) { }
+  
+  //responsavel por chamar o metodo de validação ao iniciar 
+  ngOnInit() {
+    this.gerarForm();
+  }
+
+  //metodo responsavel pelo login
+  gerarForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  //metodo de validação do login
+  logar() {
+    if (this.form.invalid) {
+      return;
+    }
+    //recebe o token da API REST redirecionando para telas de ADMIN caso o token for igual a 1,
+    //token diferente de 1 redireciona para tela de funcionario
+    const login: Login = this.form.value;
+    this.loginService.logar(login)
+      .subscribe(
+        data => {
+          localStorage['token'] = data['data']['token'];
+          const usuarioData = JSON.parse(
+            atob(data['data']['token'].split('.')[1]));
+          if (usuarioData['role'] == 'ROLE_ADMIN') {
+            this.router.navigate(['/admin']);
+          } else {
+            this.router.navigate(['/funcionario']);
+          }
+        },
+        //Tratativa de erros
+        err => {
+          let msg: string = "Tente novamente em instantes.";
+          if (err['status'] == 401) {
+            msg = "Email/senha inválido(s)."
+          }
+          this.snackBar.open(msg, "Erro", { duration: 5000 });
+        }
+      );
+  }
+}
