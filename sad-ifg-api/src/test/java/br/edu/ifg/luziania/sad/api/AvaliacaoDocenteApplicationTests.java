5242231b82b793d7c14b27f194d39764d579e388
Patrick Cavalcante <54637677+PatrickCavalcant@users.noreply.github.com>
2022-05-26 21:48:00 -0300

    Repositório teste

---
+++ sad-ifg-api/src/test/java/br/edu/ifg/luziania/sad/api/AvaliacaoDocenteApplicationTests.java
@@ -0,0 +1,18 @@
+package br.edu.ifg.luziania.sad.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AvaliacaoDocenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
