1b09c2e23e40f337fb435fd73378ffff0876259a
Patrick Cavalcante <54637677+PatrickCavalcant@users.noreply.github.com>
2022-07-06 19:02:48 -0300

    Interface de Avaliação

--- sad-ifg-client/src/app/avaliacao/components/avaliar/avaliar.component.ts
+++ sad-ifg-client/src/app/avaliacao/components/avaliar/avaliar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,22 +11,68 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import * as moment from 'moment';
 
+import {
+  Usuario,
+  UsuarioService,
+  HttpUtilService
+} from '../../../shared';
+
 @Component({
   selector: 'app-avalia',
   templateUrl: './avaliar.component.html',
   styleUrls: ['./avaliar.component.css']
 })
 export class AvaliarComponent implements OnInit {
-  private dataAtualEn: string;
+
   dataAtual: string;
 
-  constructor() {
+  usuarioId: string;
+  usuarios: Usuario[];
+  
+  @ViewChild(MatSelect, { static: true }) matSelect: MatSelect;
+  form: FormGroup;
 
+  constructor( 
+    private UsuarioService: UsuarioService,  
+    private httpUtil: HttpUtilService,
+    private snackBar: MatSnackBar,
+    private fb: FormBuilder) {
+    
    }
 
   ngOnInit(): void {
+    this.obterUsuarios();
+    this.gerarForm();
     this.dataAtual = moment().format('DD/MM/YYYY HH:mm:ss');
-  	this.dataAtualEn = moment().format('YYYY-MM-DD HH:mm:ss');
+  }
+
+  gerarForm() {
+    this.form = this.fb.group({
+      funcs: ['', []]
+    });
+  }
+
+  get funcId(): string {
+    return sessionStorage['usuarioId'] || false;
+  }
+
+  obterUsuarios() {
+    this.UsuarioService.listarUsuariosPorEmpresa()
+      .subscribe(
+        data => {
+          const usuarioId: string = this.httpUtil.obterIdUsuario();
+          this.usuarios = (data.data as Usuario[])
+            .filter(func => func.id != usuarioId); //Filto para retirada do usuário Administrador
+
+          if (this.funcId) {
+            this.form.get('funcs').setValue(parseInt(this.funcId, 10));
+          }
+        },
+        err => {
+          const msg: string = "Erro obtendo usuários.";
+          this.snackBar.open(msg, "Erro", { duration: 5000 });
+        }
+      );
   }
 
 
