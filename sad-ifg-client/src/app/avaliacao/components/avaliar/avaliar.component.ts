5e1075d6980fa17ede399413e99cf633f1187d22
Patrick Cavalcante <54637677+PatrickCavalcant@users.noreply.github.com>
2022-10-13 18:39:44 -0300

    gerarNota() responsável pela geração da media

--- sad-ifg-client/src/app/avaliacao/components/avaliar/avaliar.component.ts
+++ sad-ifg-client/src/app/avaliacao/components/avaliar/avaliar.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -17,29 +17,40 @@ import {
   HttpUtilService
 } from '../../../shared';
 
+import { Avaliacao } from '../../models';
+
+
 @Component({
   selector: 'app-avalia',
   templateUrl: './avaliar.component.html',
   styleUrls: ['./avaliar.component.css']
 })
 export class AvaliarComponent implements OnInit {
-
   dataAtual: string;
   usuarioLogadoId: string;
   usuarioId: string;
-
   usuarios: Usuario[];
   usuariosLogado: Usuario[];
-  
+
+  @ViewChild('nota1') nota1:ElementRef; 
+  @ViewChild('nota2') nota2:ElementRef; 
+  @ViewChild('nota3') nota3:ElementRef; 
+  @ViewChild('nota4') nota4:ElementRef; 
+  @ViewChild('nota5') nota5:ElementRef; 
+  @ViewChild('nota6') nota6:ElementRef; 
+  @ViewChild('nota7') nota7:ElementRef; 
+  @ViewChild('nota8') nota8:ElementRef; 
+  @ViewChild('nota9') nota9:ElementRef; 
+  @ViewChild('nota10') nota10:ElementRef; 
+
   @ViewChild(MatSelect, { static: true }) matSelect: MatSelect;
   form: FormGroup;
-
   constructor( 
     private UsuarioService: UsuarioService,  
     private httpUtil: HttpUtilService,
     private snackBar: MatSnackBar,
     private fb: FormBuilder) {
-    
+
    }
 
   ngOnInit(): void {
@@ -47,13 +58,16 @@ export class AvaliarComponent implements OnInit {
     this.gerarForm();
     this.dataAtual = moment().format('DD/MM/YYYY HH:mm:ss');
     this.getUserData();
+
   }
 
   gerarForm() {
     this.form = this.fb.group({
-      funcs: ['', []],
-      userLogado: ['', []]
+      userLogado: ['', []],
+      nota1: ['', [Validators.required]]
     });
+
+      
 
   }
 
@@ -104,4 +118,25 @@ export class AvaliarComponent implements OnInit {
     );
   }
 
+  gerarNota(){
+    const nota1 = parseFloat(this.nota1.nativeElement.value);
+    const nota2 = parseFloat(this.nota2.nativeElement.value);
+    const nota3 = parseFloat(this.nota3.nativeElement.value);
+    const nota4 = parseFloat(this.nota4.nativeElement.value);
+    const nota5 = parseFloat(this.nota5.nativeElement.value);
+    const nota6 = parseFloat(this.nota6.nativeElement.value);
+    const nota7 = parseFloat(this.nota7.nativeElement.value);
+    const nota8 = parseFloat(this.nota8.nativeElement.value);
+    const nota9 = parseFloat(this.nota9.nativeElement.value);
+    const nota10 = parseFloat(this.nota10.nativeElement.value);
+    const media: number[] = Array();
+
+    let valor: number;
+
+    valor = ((nota1+nota2+nota3+nota4+nota5+nota6+nota7+nota8+nota9+nota10)/10);
+    console.log(valor);
+
+    media.push(valor);
+    return media;
+  }
 }
