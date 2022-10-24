b76e45d0e132a05aa19d4f28544223d68773e167
Patrick Cavalcante <54637677+PatrickCavalcant@users.noreply.github.com>
2022-10-24 14:40:01 -0300

    ajuste no tipo, campo nota

--- sad-ifg-client/src/app/shared/models/lancamento.model.ts
+++ sad-ifg-client/src/app/shared/models/lancamento.model.ts
@@ -3,7 +3,7 @@ export class Lancamento {
 
 	constructor(public data: string,
 				public tipo: string,
-				public nota: string,
+				public nota: Number,
 				public periodo: string,
 				public usuarioId: string,
 				public id?: string) {}
